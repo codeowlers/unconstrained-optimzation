931b3afc3420feb7ada713ec609f960e3c427663
Ali Yassine <aliyassine_2000@hotmail.com>
2023-02-08 22:59:42 +0100

    Extended Rosenbrock

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -4,8 +4,9 @@ from .hessian import hessian
 from .global_methods import newton, newton_back_tracking, steepest_descent, steepest_descent_back_tracking
 from .chained_rosenbrock import chained_rosenbrock
 from .extended_rosenbrock import extended_rosenbrock
+from .hyperparam_tuning import grid_search_steepest_descent
 
 
 
 
-__all__ = ['rosenbrock', 'gradient', 'hessian', 'newton', 'newton_back_tracking','steepest_descent','steepest_descent_back_tracking','chained_rosenbrock','extended_rosenbrock']
+__all__ = ['rosenbrock', 'gradient', 'hessian', 'newton', 'newton_back_tracking','steepest_descent','steepest_descent_back_tracking','chained_rosenbrock','extended_rosenbrock','grid_search_steepest_descent']
