57c0fb8aa68b692683fd0746251f955fba013e54
Ali Yassine <aliyassine_2000@hotmail.com>
2023-02-08 19:34:20 +0100

    Organize directories

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,6 +1,6 @@
 from .rosenbrock_method import rosenbrock
 from .gradient import gradient
-from .hessian_method import hessian
+from .hessian import hessian
 from .global_methods import newton, steepest_descent
 
 __all__ = ['rosenbrock', 'gradient', 'hessian', 'newton', 'steepest_descent']
--- algorithms/hessian.py
+++ algorithms/hessian.py
@@ -1,12 +1,5 @@
 import numpy as np
-from algorithms import rosenbrock, gradient
-
-
-# Define the Hessian matrix of the Rosenbrock function
-def hessian_rosenbrock(x):
-    # Return the Hessian matrix of the Rosenbrock function for input x
-    return np.array([[-400 * (x[1] - 3 * x[0] ** 2) + 2, -400 * x[0]],
-                     [-400 * x[0], 200]])
+from . import gradient 
 
 def hessian(f, x0, epsilon=1e-4):
     n = len(x0)
