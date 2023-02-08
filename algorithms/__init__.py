588b0307eff3c6d98545afaaa8a94b160edde00f
Hadi Ibrahim <17hadiibrahim@gmail.com>
2023-02-08 22:10:20 +0100

    add grid search

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -2,7 +2,8 @@ from .rosenbrock_method import rosenbrock
 from .gradient import gradient
 from .hessian import hessian
 from .global_methods import newton, newton_back_tracking, steepest_descent, steepest_descent_back_tracking
+from .hyperparam_tuning import grid_search_steepest_descent
 
 
 
-__all__ = ['rosenbrock', 'gradient', 'hessian', 'newton', 'newton_back_tracking','steepest_descent','steepest_descent_back_tracking']
+__all__ = ['rosenbrock', 'gradient', 'hessian', 'newton', 'newton_back_tracking','steepest_descent','steepest_descent_back_tracking', 'grid_search_steepest_descent']
