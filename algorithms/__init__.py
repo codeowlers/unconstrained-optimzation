e57eb51993f9d6c83ab18b1509951796e206857e
Ali Yassine <aliyassine_2000@hotmail.com>
2023-02-08 21:24:27 +0100

    Add newton with backtracking

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,8 +1,8 @@
 from .rosenbrock_method import rosenbrock
 from .gradient import gradient
 from .hessian import hessian
-from .global_methods import newton, steepest_descent
+from .global_methods import newton, newton_back_tracking, steepest_descent, steepest_descent_back_tracking
 
 
 
-__all__ = ['rosenbrock', 'gradient', 'hessian', 'newton', 'steepest_descent']
+__all__ = ['rosenbrock', 'gradient', 'hessian', 'newton', 'newton_back_tracking','steepest_descent','steepest_descent_back_tracking']
--- algorithms/global_methods.py
+++ algorithms/global_methods.py
@@ -1,13 +1,11 @@
 import numpy as np
 import time
-
 from algorithms import hessian, gradient, rosenbrock
 
-
-def backtracking(x, d, f, g, alpha=0.5, beta=0.0001):
+def backtracking(x, d, f, g, c=0.0001, rho=0.5):
     t = 1
-    while f(x + t * d) > f(x) + beta * t * np.dot(g, d):
-        t *= alpha
+    while f(x + t * d) > f(x) + c * t * np.dot(g, d):
+        t *= rho
     return t
 
 
@@ -38,8 +36,29 @@ def newton(x0, epsilon=1e-8, max_iter=100):
     # Return the optimal point x, the number of iterations, and the elapsed time
     return x, iter, end_time - start_time
 
-# Define the function `steepest_descent` with four input arguments
-def steepest_descent(x0, learning_rate=0.00001, epsilon=1e-8, max_iter=1000):
+# Define the Newton method with backtracking line search for finding the minimum of a function
+def newton_back_tracking(x0, epsilon=1e-8, max_iter=100, c=1e-4, rho=0.5):
+    x = x0
+    gradient_x = gradient(rosenbrock, x)
+    iter = 0
+    start_time = time.time()
+    while np.linalg.norm(gradient_x) >= epsilon and iter < max_iter:
+        iter += 1
+        hessian_matrix = hessian(rosenbrock, x)
+        try:
+            inverse = np.linalg.inv(hessian_matrix)
+        except np.linalg.LinAlgError:
+            inverse = np.linalg.pinv(hessian_matrix)
+        dx = -inverse.dot(gradient_x)
+        t = backtracking(x, dx, rosenbrock, gradient_x, c, rho)
+        x = x + t * dx
+        gradient_x = gradient(rosenbrock, x)
+    end_time = time.time()
+    return x, iter, end_time - start_time
+
+
+# Define the function `steepest_descent_back_tracking` with five input arguments
+def steepest_descent_back_tracking(x0 , epsilon=1e-8, max_iter=100, c=1e-4, rho=0.5):
     x = x0
     gradient_rosenbrock = gradient(rosenbrock, x)
     # Initialize the iteration count to 0
@@ -51,12 +70,32 @@ def steepest_descent(x0, learning_rate=0.00001, epsilon=1e-8, max_iter=1000):
         # Increase the iteration count by 1
         iter += 1
         # Compute negative gradient 
-        d = -gradient_rosenbrock
+        dx = -gradient_rosenbrock
         # Update the variable `x` using the gradient and the learning rate
-        t = backtracking(x, d, rosenbrock, gradient_rosenbrock)
-        x = x + t * d
+        t = backtracking(x, dx, rosenbrock, gradient_rosenbrock, c, rho)
+        x = x + t * dx
 
     # Record the ending time
     end_time = time.time()
     # Return the updated variable `x`, the number of iterations, and the total time taken
+    return x, iter, end_time - start_time
+
+
+# Define the function `steepest_descent` with four input arguments
+def steepest_descent(x0, learning_rate=0.01, epsilon=1e-8, max_iter=100):
+    # Initialize the input argument x0 as the variable x
+    x = x0
+    # Initialize the iteration count to 0
+    iter = 0
+    # Record the starting time
+    start_time = time.time()
+    # Loop until either the gradient norm is less than epsilon or the number of iterations reaches max_iter
+    while np.linalg.norm(gradient(x)) >= epsilon and iter < max_iter:
+        # Increase the iteration count by 1
+        iter += 1
+        # Update the variable x using the gradient and the learning rate
+        x = x - learning_rate * gradient(x)
+    # Record the ending time
+    end_time = time.time()
+    # Return the updated variable x, the number of iterations, and the total time taken
     return x, iter, end_time - start_time
