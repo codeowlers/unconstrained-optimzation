8a8365bd39b04476eb815eef9f0845f6a1aff766
Hadi Ibrahim <17hadiibrahim@gmail.com>
2023-02-09 22:07:51 +0100

    add inexact newton method

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,7 +1,7 @@
 from .rosenbrock import rosenbrock, rosenbrock_gradient, rosenbrock_hessian
 from .gradient import gradient
 from .hessian import hessian
-from .global_methods import newton, newton_back_tracking, steepest_descent, steepest_descent_back_tracking
+from .global_methods import newton, newton_back_tracking, steepest_descent, steepest_descent_back_tracking, inexact_newton_back_tracking
 from .chained_rosenbrock import chained_rosenbrock, chained_rosenbrock_gradient ,chained_rosenbrock_hessian
 from .extended_rosenbrock import extended_rosenbrock
 from .hyperparam_tuning import grid_search_steepest_descent
@@ -9,4 +9,4 @@ from .problem76 import problem76, problem76_gradient, problem76_hessian
 from .utils import *
 from .generalized_brown import generalized_brown, generalized_brown_gradient, generalized_brown_hessian
 
-__all__ = ['rosenbrock','rosenbrock_hessian', 'rosenbrock_gradient', 'gradient', 'hessian', 'newton', 'newton_back_tracking','steepest_descent','steepest_descent_back_tracking','chained_rosenbrock' ,'chained_rosenbrock_gradient' ,'chained_rosenbrock_hessian' ,'extended_rosenbrock','grid_search_steepest_descent','problem76', 'problem76_gradient', 'problem76_hessian','results_out','generalized_brown', 'generalized_brown_gradient', 'generalized_brown_hessian' ]
+__all__ = ['rosenbrock','rosenbrock_hessian', 'rosenbrock_gradient', 'gradient', 'hessian', 'newton', 'newton_back_tracking','steepest_descent','steepest_descent_back_tracking','chained_rosenbrock' ,'chained_rosenbrock_gradient' ,'chained_rosenbrock_hessian' ,'extended_rosenbrock','grid_search_steepest_descent','problem76', 'problem76_gradient', 'problem76_hessian','results_out','generalized_brown', 'generalized_brown_gradient', 'generalized_brown_hessian', 'inexact_newton_back_tracking' ]
