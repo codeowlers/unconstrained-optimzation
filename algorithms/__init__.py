a941bab0fa9a0eb28abed6de5831e280b4c8f0bd
developer-sidani <developer.sidani@gmail.com>
2023-02-09 17:39:16 +0100

    feat(results): results printing method

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -6,7 +6,7 @@ from .chained_rosenbrock import chained_rosenbrock
 from .extended_rosenbrock import extended_rosenbrock
 from .hyperparam_tuning import grid_search_steepest_descent
 from .problem76 import problem76, problem76_gradient, problem76_hessian
+from .utils import *
 
 
-
-__all__ = ['rosenbrock','rosenbrock_hessian', 'rosenbrock_gradient', 'gradient', 'hessian', 'newton', 'newton_back_tracking','steepest_descent','steepest_descent_back_tracking','chained_rosenbrock','extended_rosenbrock','grid_search_steepest_descent','problem76', 'problem76_gradient', 'problem76_hessian']
+__all__ = ['rosenbrock','rosenbrock_hessian', 'rosenbrock_gradient', 'gradient', 'hessian', 'newton', 'newton_back_tracking','steepest_descent','steepest_descent_back_tracking','chained_rosenbrock','extended_rosenbrock','grid_search_steepest_descent','problem76', 'problem76_gradient', 'problem76_hessian','results_out']
---
+++ algorithms/utils.py
@@ -0,0 +1,4 @@
+def results_out(method, x_opt, iter, elapsed_time):
+    print(f'Optimal point ({method}): x_opt ={x_opt}')
+    print(f'Number of iterations ({method}):{iter}')
+    print(f'Elapsed time ({method}): {elapsed_time} seconds')
