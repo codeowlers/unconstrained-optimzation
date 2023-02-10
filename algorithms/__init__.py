56c350e909f76638ffc062aed35a2f56abb95e24
Ali Yassine <aliyassine_2000@hotmail.com>
2023-02-10 15:09:27 +0100

    Combine plot.py  with results_out.py

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -3,11 +3,10 @@ from .gradient import gradient
 from .hessian import hessian
 from .global_methods import newton, newton_back_tracking, steepest_descent, steepest_descent_back_tracking, inexact_newton_back_tracking
 from .chained_rosenbrock import chained_rosenbrock, chained_rosenbrock_gradient ,chained_rosenbrock_hessian
-from .extended_rosenbrock import extended_rosenbrock
 from .hyperparam_tuning import grid_search_steepest_descent
 from .problem76 import problem76, problem76_gradient, problem76_hessian
 from .utils import *
 from .generalized_brown import generalized_brown, generalized_brown_gradient, generalized_brown_hessian
 from .plot import plot
 
-__all__ = ['rosenbrock','rosenbrock_hessian', 'rosenbrock_gradient', 'gradient', 'hessian', 'newton', 'newton_back_tracking','steepest_descent','steepest_descent_back_tracking','chained_rosenbrock' ,'chained_rosenbrock_gradient' ,'chained_rosenbrock_hessian' ,'extended_rosenbrock','grid_search_steepest_descent','problem76', 'problem76_gradient', 'problem76_hessian','results_out','generalized_brown', 'generalized_brown_gradient', 'generalized_brown_hessian', 'inexact_newton_back_tracking', 'plot' ]
+__all__ = ['rosenbrock','rosenbrock_hessian', 'rosenbrock_gradient', 'gradient', 'hessian', 'newton', 'newton_back_tracking','steepest_descent','steepest_descent_back_tracking','chained_rosenbrock' ,'chained_rosenbrock_gradient' ,'chained_rosenbrock_hessian' ,'grid_search_steepest_descent','problem76', 'problem76_gradient', 'problem76_hessian','results_out','generalized_brown', 'generalized_brown_gradient', 'generalized_brown_hessian', 'inexact_newton_back_tracking', 'plot' ]
