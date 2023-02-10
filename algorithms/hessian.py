670838b1c11d579c07c6c920c69fb2f487c35baa
developer-sidani <developer.sidani@gmail.com>
2023-02-10 22:50:22 +0100

    docs(hessian): hessian function documentation

--- algorithms/hessian.py
+++ algorithms/hessian.py
@@ -3,13 +3,19 @@ from . import gradient
 
 
 def hessian(f, x0, epsilon=1e-4):
+    # Initialize the size of hessian to be nxn where n is the size of x0
     n = len(x0)
     hessian = np.zeros((n, n))
     for i in range(n):
+        # Create a copy of x0 with i-th element incremented by epsilon
         x_plus = np.copy(x0)
         x_plus[i] += epsilon
+        # Create a copy of x0 with i-th element decremented by epsilon
         x_minus = np.copy(x0)
         x_minus[i] -= epsilon
+        # Calculate the gradient difference between x_plus and x_minus
         gradient_i = gradient(f, x_plus) - gradient(f, x_minus)
+        # Approximate the i-th row of the hessian matrix
         hessian[i] = gradient_i / (2 * epsilon)
+    # Return the approximate hessian matrix
     return hessian
