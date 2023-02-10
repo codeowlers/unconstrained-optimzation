0f87dd6c8e6b096609162d351c78cbb1719d6810
developer-sidani <developer.sidani@gmail.com>
2023-02-10 23:03:26 +0100

    docs(utils): util functions documentation

--- algorithms/utils.py
+++ algorithms/utils.py
@@ -3,6 +3,19 @@ from .plot import plot
 
 
 def results_out(method, x_opt, iter, elapsed_time, function, gradient_f, x_vals, plotGraph=False):
+    """
+    Print results of an optimization method.
+
+    Parameters:
+    method (str): Name of the optimization method.
+    x_opt (np.array): Optimal point found by the optimization method.
+    iter (int): Number of iterations required to reach the optimal point.
+    elapsed_time (float): Time taken by the optimization method to reach the optimal point.
+    function (function): Function to be optimized.
+    gradient_f (function): Gradient of the function to be optimized.
+    x_vals (np.array): Initial points to be used in the optimization method.
+    plotGraph (bool, optional): Whether to plot the optimization result. Defaults to False.
+    """
     print(f"COST of {method}", function(x_opt))
     gradient_value = gradient_f(x_opt)
     print("GRADIENT NORM:", np.linalg.norm(gradient_value))
