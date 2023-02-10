cd15101571fcee8d5d01cc2b4e1b8f29b0df85c9
developer-sidani <developer.sidani@gmail.com>
2023-02-10 22:55:56 +0100

    docs(hyperparams)!: hyperparam tuning functions documentation
    BREAKING CHANGE: Add scikit-learn package to the requirements.txt file

--- algorithms/hyperparam_tuning.py
+++ algorithms/hyperparam_tuning.py
@@ -1,23 +1,31 @@
 import numpy as np
-# from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error
 
 from algorithms import steepest_descent_back_tracking, rosenbrock
 
 
-def grid_search(x0, f, g, learning_rates, epsilons, max_iters):
+def grid_search(x0, f, g, learning_rates, epsilons, max_iters, method):
+    # Initialize the best parameters and best mean squared error to keep track of the best optimization result
     best_params = None
     best_mse = float('inf')
+    # Loop over all possible combinations of learning rates, epsilons, and max_iters
     for learning_rate in learning_rates:
         for epsilon in epsilons:
             for max_iter in max_iters:
-                x, iter, time_elapsed = steepest_descent(x0, learning_rate, epsilon, max_iter)
-                mse = mean_squared_error(x, np.array([1, 1]))  # target is the true value of x
+                # Call the optimization method (e.g. steepest descent or Newton)
+                x, iter, time_elapsed = method(x0, learning_rate, epsilon, max_iter)
+                # Calculate the mean squared error between the optimized solution and the true value
+                mse = mean_squared_error(x, np.array([1, 1]))
+                # Update the best parameters and best mean squared error if the current result is better
                 if mse < best_mse:
                     best_mse = mse
                     best_params = (epsilon, max_iter)
+    # Return the best optimization parameters and the best mean squared error
     return best_params, best_mse
 
 
+# Initialize the best result, learning rate, backtracking rate, optimized solution, iteration, elapsed time,
+# and the respective best values
 def grid_search_steepest_descent(x0):
     # Find the optimal point, number of iterations, and elapsed time using the Newton method
     c_range = [1e-5, 1e-4, 1e-3, 1e-2, 1e-1]
@@ -29,11 +37,14 @@ def grid_search_steepest_descent(x0):
     best_x_opt = None
     best_iter = None
     best_elapsed_time = None
-
+    # Loop over all possible combinations of the learning rate and backtracking rate
     for c in c_range:
         for rho in rho_range:
+            # Call the steepest descent optimization method with backtracking line search
             x_opt, iter, elapsed_time = steepest_descent_back_tracking(x0, c, rho)
+            # Evaluate the result of the optimization method
             result = rosenbrock(x_opt)
+            # Update the best parameters and results if the current result is better
             if result < best_result:
                 best_result = result
                 best_c = c
@@ -41,7 +52,7 @@ def grid_search_steepest_descent(x0):
                 best_x_opt = x_opt
                 best_iter = iter
                 best_elapsed_time = elapsed_time
-
+    # Return the best optimized solution, number of iterations, elapsed time, learning rate, and backtracking rate
     return best_x_opt, best_iter, best_elapsed_time, best_c, best_rho
 
 # Define the range of values for each hyperparameter
